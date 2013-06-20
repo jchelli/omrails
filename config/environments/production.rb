0adeae8d38d99ff8a9dd7d5a0d08341e70028481
Jordan Chelli <jordan.chelli@gmail.com>
2013-06-20 22:16:34 +0200

    International user Amazon S3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Omrails::Application.configure do
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
+
+# config/initializers/paperclip.rb 
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 end
